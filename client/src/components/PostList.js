c1007b1c9741cc3ac07f4d97127698b537e1f5b4
Donghyeok Yoo <robin14dev@gmail.com>
2022-08-02 09:44:32 +0900

    Fix : Delete caches in git not to upload .env file

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -1,8 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, {useState } from "react";
 import styled from "styled-components";
-import axios from "axios";
 import {useNavigate} from "react-router-dom"
-import Search from "./Search";
 
 
 
